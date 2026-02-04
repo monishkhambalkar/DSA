1a2f14e935cd93c2a3e78acddba54e6c90173fd5
Monish Khambalkar <monish.khambalkar@plus91.in>
2026-02-04 12:00:02 +0530

    Auto commit on 2026-02-04 12:00:02

---
+++ palidrom_fibonacchi_factorial_prime_number_anagram.js
@@ -0,0 +1,44 @@
+// NOTES
+
+//1. PALINDROME
+    // What is Palindrome?
+        // A word/number that reads the same forward and backward.
+        // Examples:
+        // madam, racecar, 121, 1221
+    
+    // Approach to solve
+        // Two common approaches:
+            // Reverse the string and compare
+            // Compare first & last characters moving inward
+
+// 2. FIBONACCI
+    // What is Fibonacci?
+        // A series where each number is sum of previous two:
+        // 0, 1, 1, 2, 3, 5, 8, 13...
+        // F(n) = F(n-1) + F(n-2)
+    // Approach to solve
+        // Recursive
+
+// 3. FACTORIAL
+    // What is Factorial?
+        // Multiplication of all numbers from 1 to N.
+        // 5! = 5 × 4 × 3 × 2 × 1 = 120
+    // Approach
+        // Multiply numbers from 1 to N
+    
+//4. PRIME NUMBER
+    // What is Prime?
+        // A number divisible only by:
+        // 1 and itself
+        // Examples: 2,3,5,7,11,13
+        // Not prime: 4,6,8,9
+
+// 5. ANAGRAM
+    // What is Anagram?
+        // Two strings having same letters in different order
+        // Examples:
+            // listen ↔ silent
+            // evil ↔ vile
+    // Approach
+        // Sort both strings and compare
+        // Count characters

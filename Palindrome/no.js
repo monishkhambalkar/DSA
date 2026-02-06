2d75230bc1a47cd5f84f0da01351f80d8d308568
Monish Khambalkar <monish.khambalkar@plus91.in>
2026-02-06 12:00:02 +0530

    Auto commit on 2026-02-06 12:00:02

---
+++ Palindrome/no.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+var validPalindrome = function(s) {
+    let left  = 0;
+    let right  = s.length - 1;
+    while(left < right){
+        if(s[left] !== s[right]){
+            return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
+        }
+        left++;
+        right--;
+        
+    }
+    return true;
+}
+function isPalindrome(str, left, right){
+    while(left < right){
+        if(str[left] !== str[right]){
+            return false
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+console.log(validPalindrome("abca"))
+console.log(validPalindrome("aba"))
+console.log(validPalindrome("abc"))

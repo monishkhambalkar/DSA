c139f5466722720a8cec91601f0efeb362f86ea8
Monish Khambalkar <monish.khambalkar@plus91.in>
2026-03-05 12:00:02 +0530

    Auto commit on 2026-03-05 12:00:02

---
+++ approach/two_pointer/q1.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+
+    let start = 0;
+    let end = numbers.length - 1;
+
+    while(start < end){
+
+        let sum = numbers[start] + numbers[end];
+
+        if(sum === target){
+            return [start + 1, end + 1];
+        }
+
+        if(sum < target){
+            start++;
+        }else{
+            end--;
+        }
+    }
+
+};

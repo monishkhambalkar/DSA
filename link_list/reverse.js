6e3052688fd13934c976026f0dfefc97a142bf5a
Monish Khambalkar <monish.khambalkar@plus91.in>
2026-03-26 12:00:02 +0530

    Auto commit on 2026-03-26 12:00:02

--- link_list/reverse.js
+++ link_list/reverse.js
@@ -5,7 +5,7 @@
 //     }
 // }
 
-const { createRef } = require("react");
+// const { createRef } = require("react");
 
 // class LinkedList{
 //     constructor(){
@@ -46,62 +46,138 @@ const { createRef } = require("react");
 
 
 
-function reverseList(head){
-    console.log(head);
+// function reverseList(head){
+//     console.log(head);
 
-    let prev = null;
-    let current = head;
+//     let prev = null;
+//     let current = head;
 
-    while(current){
-        let xx   = current.next;
-        current.next = prev;
-        prev = current;
-        current = newNode;
-    }
-}
-
-console.log(reverseList([1,2,3,4,5]))
+//     while(current){
+//         let xx   = current.next;
+//         current.next = prev;
+//         prev = current;
+//         current = newNode;
+//     }
+// }
 
 
 
 
-var reverseList = function(head) {
-    let prev = null;
-    let current = head;
-    console.log(" head ",head)
-    while(current){
-        let nextNode = current.next; 
-        console.log(" nextNode ",nextNode)
-        current.next = prev;  
-        console.log(" current.next ",current.next)       
-        prev = current;    
-        console.log(" prev ",prev)             
-        current = nextNode;  
-        console.log()        
-    }
-    return prev; // new head
-};
 
 
- head  [1,2,3,4,5]
+// var reverseList = function(head) {
+//     let prev = null;
+//     let current = head;
+//     console.log(" head ",head)
+//     while(current){
+//         let nextNode = current.next; 
+//         console.log(" nextNode ",nextNode)
+//         current.next = prev;  
+//         console.log(" current.next ",current.next)   
+//         console.log(" current ",current)       
+//         prev = current;    
+//         console.log(" prev ",prev)             
+//         current = nextNode;  
+//         console.log()        
+//     }
+//     return prev; // new head
+// };
 
- 
- nextNode  [2,3,4,5]
- current.next  null
- prev  [1]
 
- nextNode  [3,4,5]
- current.next  [1]
- prev  [2,1]
+// console.log(reverseList([1,2,3,4,5]))
 
- nextNode  [4,5]
- current.next  [2,1]
- prev  [3,2,1]
+//  head  [1,2,3,4,5]
 
- nextNode  [5]
- current.next  [3,2,1]
- prev  [4,3,2,1]
 
- nextNode  null
- current.next  [4,3,2,1]
- prev  [5,4,3,2,1]
+//  nextNode  [2,3,4,5]
+//  current.next  null
+//  current  [1]
+//  prev  [1]
+
+//  nextNode  [3,4,5]
+//  current.next  [1]
+//  current  [2,1]
+//  prev  [2,1]
+
+//  nextNode  [4,5]
+//  current.next  [2,1]
+//  current  [3,2,1]
+//  prev  [3,2,1]
+
+//  nextNode  [5]
+//  current.next  [3,2,1]
+//  current  [4,3,2,1]
+//  prev  [4,3,2,1]
+
+//  nextNode  null
+//  current.next  [4,3,2,1]
+//  current  [5,4,3,2,1]
+//  prev  [5,4,3,2,1]
+
+
+
+
+
+
+
+
+
+//  function reverseList(head){
+//     let prev = null;
+//     let current = head
+//     while(current){
+//         let newNode = current.next
+//         console.log("newNode ", newNode)
+//         return false
+//     }
+//  }
+
+//  console.log(reverseList([1,2,3,4,5]))
+
+
+
+
+//  newNode  [2,3,4,5]
+// current  [1,2,3,4,5]
+// prev  null
+// current.next  null
+// current  [1]
+//  prev  [1]
+// current  [2,3,4,5]
+
+
+// newNode  [3,4,5]
+// current  [2,3,4,5]
+// prev  [1]
+// current.next  [1]
+// current  [2,1]
+//  prev  [2,1]
+// current  [3,4,5]
+
+
+// newNode  [4,5]
+// current  [3,4,5]
+// prev  [2,1]
+// current.next  [2,1]
+// current  [3,2,1]
+//  prev  [3,2,1]
+// current  [4,5]
+
+
+// newNode  [5]
+// current  [4,5]
+// prev  [3,2,1]
+// current.next  [3,2,1]
+// current  [4,3,2,1]
+//  prev  [4,3,2,1]
+// current  [5]
+
+
+// newNode  null
+// current  [5]
+// prev  [4,3,2,1]
+// current.next  [4,3,2,1]
+// current  [5,4,3,2,1]
+//  prev  [5,4,3,2,1]
+// current  null
+
